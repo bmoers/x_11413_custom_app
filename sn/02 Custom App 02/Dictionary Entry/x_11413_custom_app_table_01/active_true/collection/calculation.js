87391981490c6c659f0400fe8fd4ddad551335ed
System Administrator <boris_moers@rcomext.com>
2021-02-17 13:11:11 +0100

    Changes by 'System Administrator' on '02 > Custom App 02' -- Build #1

---
+++ sn/02 Custom App 02/Dictionary Entry/x_11413_custom_app_table_01/active_true/collection/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : 02 > Custom App 02
+ * ClassName   : sys_dictionary
+ * Created On  : 2020-12-04 10:56:01
+ * Created By  : admin
+ * Updated On  : 2020-12-04 10:56:01
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=dd51a27a2f3c6450d8455aab2799b67b
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
---
+++ sn/02 Custom App 02/Dictionary Entry/x_11413_custom_app_table_01/active_true/teste_2/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : 02 > Custom App 02
+ * ClassName   : sys_dictionary
+ * Created On  : 2020-12-04 10:57:03
+ * Created By  : admin
+ * Updated On  : 2020-12-04 10:57:03
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=4681a67a2f3c6450d8455aab2799b696
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
